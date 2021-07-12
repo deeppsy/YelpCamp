772441d7cbe60a41c178668f15af561ed8dfe212
Dipo <adanikindipo@gmail.com>
2021-07-12 15:19:24 +0100

    add start script

--- index.js
+++ index.js
@@ -167,6 +167,7 @@ app.use((err, req, res, next) => {
   // res.send("Sorry, error");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
